b9de7cf70481ce8fb06ba26d539fa421d90af74e
JGibson2019 <jacqueline.gibson@microsoft.com>
2022-09-08 11:22:01 -0700

    functionality changes

--- packages/commons/test/FileAxeScriptProviderTest.cs
+++ packages/commons/test/FileAxeScriptProviderTest.cs
@@ -27,26 +27,21 @@ namespace Deque.AxeCore.Commons.Test
         }
 
         [Test]
-        public void ConstructorPassedNullOrEmptyString()
+        public void ConstructorPassedInvalidStrings()
         {
             Assert.Throws<ArgumentNullException>(() =>
             {
                 var scriptProvider = new FileAxeScriptProvider(null);
             });
 
-            Assert.Throws<ArgumentNullException>(() =>
+            Assert.Throws<ArgumentException>(() =>
             {
                 var scriptProvider = new FileAxeScriptProvider("");
             });
-        }
-
-        [Test]
-        public void ConstructorPassedNonexistentFile()
-        {
-            Assert.Throws<ArgumentException>(() =>
+            
+             Assert.Throws<ArgumentException>(() =>
             {
                 var scriptProvider = new FileAxeScriptProvider("sample.html");
-                scriptProvider.Should().NotBeNull();
             });
         }
 
